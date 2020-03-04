bd1595589bd144a1b1e174556564d39467772b2d
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-03-04 10:57:31 -0800

    added new animations and updated some information.

--- app.js
+++ app.js
@@ -1,23 +1,47 @@
 const header = document.querySelector("#header");
 const mobileMenu = document.querySelector(".burger-menu");
 const mobileNav = document.querySelector(".mobile-nav");
+const navlinks = document.querySelector(".logo-n-links");
 
 let count = 1;
+const width = window.matchMedia("(max-width: 870px)");
 
 function changeImg() {
   header.style.backgroundImage = `url(./img/img${count}.jpg)`;
   setTimeout(() => {
     count++;
   }, 1000);
-  if (count === 6) {
+  if (count === 5) {
     count = 1;
   }
-  console.log(count);
 }
 
-// setInterval(changeImg, 5000);
+function mobile() {
+  if (width.matches) {
+    header.style.backgroundImage = `url(./img/img5.jpg)`;
+  } else {
+    setInterval(changeImg, 5000);
+  }
+}
+
+mobile();
+width.addListener(mobile);
 
 mobileMenu.addEventListener("click", () => {
   mobileMenu.classList.toggle("close");
   mobileNav.classList.toggle("show");
+});
+
+window.addEventListener("scroll", e => {
+  e.preventDefault();
+
+  if (window.scrollY <= 5000) {
+    navlinks.style.backgroundColor = "rgba(115, 195, 40, 0.8)";
+  } else {
+    navlinks.style.backgroundColor = "transparent";
+  }
+
+  if (window.scrollY === 0) {
+    navlinks.style.backgroundColor = "transparent";
+  }
 });
