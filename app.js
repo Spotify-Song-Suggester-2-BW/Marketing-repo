71821ef3afcfef8372fc3e0e20038347dbcb993e
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-03-05 11:01:50 -0800

    testing

--- app.js
+++ app.js
@@ -7,13 +7,12 @@ let count = 1;
 const width = window.matchMedia("(max-width: 870px)");
 
 function changeImg() {
-  //   header.style.backgroundImage = `url(./img/img${count}.jpg)`;
-  count++;
+  header.style.backgroundImage = `url(./img/img${count}.jpg)`;
   setTimeout(() => {
-    header.style.backgroundImage = `url(./img/img${count}.jpg)`;
-    //  count++;
-  }, 1);
-  if (count === 6) {
+    //  header.style.backgroundImage = `url(./img/img${count}.jpg)`;
+    count++;
+  }, 100);
+  if (count === 5) {
     count = 1;
   }
   console.log(count);
