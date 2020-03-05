e78bc16edfb8b8af5c8396390a396034b6e69f1b
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-03-05 11:48:10 -0800

    testing

--- app.js
+++ app.js
@@ -6,17 +6,12 @@ const navlinks = document.querySelector(".logo-n-links");
 let count = 1;
 const width = window.matchMedia("(max-width: 870px)");
 
-header.style.backgroundImage = `url(./img/img${count}.jpg)`;
 function changeImg() {
   header.style.backgroundImage = `url(./img/img${count}.jpg)`;
   count++;
 
   header.style.backgroundImage = `url(./img/img${count}.jpg)`;
 
-  setTimeout(() => {
-    //  header.style.backgroundImage = `url(./img/img${count}.jpg)`;
-    //  count++;
-  }, 1);
   if (count === 5) {
     count = 1;
   }
